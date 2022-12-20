c90ad3dba5b7a8e40998e6a29424d2b6dc4eb402
Manisha Shrestha <shresthamanisha79@gmail.com>
2022-12-20 16:09:37 +0545

    deleted the items by id

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,20 @@
+import React from 'react'
 import {FaTrashAlt} from "react-icons/fa"
 import '../App.css'
 
 const Card = (props) => {
+
+    const deleteItem = () =>{
+        props.delItem()
+    }
+
+
     return(
         <div className="main-card-div">
-            <h3 style={{backgroundColor: '#FF5733', color: 'white', width:500, height:50, margin:'auto', marginBottom:20}}>{props.item} <FaTrashAlt style={{color:'white', marginTop:12, float:"right"}}/></h3>
+            <h3 style={{backgroundColor: '#7cabb5', color: 'white', display:'flex', justifyContent:"space-between", width:500, height:50, margin:'auto', marginBottom:20}}>
+                {props.item} 
+                <FaTrashAlt onClick={()=>{deleteItem()}} style={{color:'white', marginTop:12, float:"right"}}/>
+            </h3>
             
         </div>
     )
--- src/page/home.js
+++ src/page/home.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react'
 import Card from '../components/card'
+import {FaPlus} from "react-icons/fa"
+import '../App.css'
 
 
 const Homepage = () => {
 
     const [inputItem, setInputItem] = useState('')
-    const [addItem, setAddItem] = useState([])
+    const [items, setItems] = useState([])
 
     const handleChange = (e) =>{
         setInputItem(e.target.value)
@@ -13,30 +15,40 @@ const Homepage = () => {
 
     const addtoList = () =>{  
         if(inputItem){
-            setAddItem([...addItem, inputItem])
+            setItems([...items, inputItem])
             setInputItem('')
         }else{
             alert("List cannot be empty")
         }
     }
 
+    const delItem = (a) =>{
+        console.log(a)
+        const filterItem = items.filter((item, id)=>{
+            if(a !== id){
+                return item
+            }
+        })
+        setItems(filterItem)
+    }
+
 
     return(
-        <div classname="homePage" style={{ backgroundColor:'#EDEADE', height: 600}}>
-            <h1 style={{color:'#FF5733'}}>To Do List</h1>
+        <div className="homePage" style={{ backgroundColor:'#e7e7e7', height: 600}}>
+            <h1 style={{color:'#7cabb5'}}>To Do List</h1>
             <div className='inputItem'>
-                <input classname="input"
+                <input className="input"
                     onChange={(e)=>{handleChange(e)}} 
                     value={inputItem}
                     type="text"
-                    placeholder="Type your todo list..." 
-                    style={{height: 50, width: 400, marginBottom:20}}/>
-                <button classname="button" 
+                    placeholder="Enter your todo list..." 
+                    style={{height: 50, width: 400, backgroundColor:'#f0f0f0', marginBottom:15, fontSize:20}}/>
+                <FaPlus className="button" 
                     onClick={()=>{addtoList()}}
-                    style={{height: 57, width: 57}}>+
-                </button>
-                {addItem.map((item, id)=>{
-                    return(<Card item={item}/>)
+                    style={{height: 20, width: 20}}
+                />
+                {items.map((item, id)=>{
+                    return(<Card item={item} delItem={()=>{delItem(id)}}/>)
                 })}
             </div>
 
